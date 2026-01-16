50965330e200b7683fb8e2a6c800ab458993db53
theHARI962n <rajul1874@gmail.com>
2026-01-16 23:18:37 +0530

    blog update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,6 +5,9 @@ const Blog = () => {
   return (
     <div className="text-white min-h-screen flex items-center justify-center bg-[#0d1117] text-2xl">
       Building in progress...will be updated soon
+      <p>
+        Meanwhile, check out my articles on medium platform.
+      </p>
     </div>
   );
 };
